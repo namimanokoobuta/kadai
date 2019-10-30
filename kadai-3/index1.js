6e756cc06a83ac4ea7f1579a6ec7dedeb80813ee
namimanokoobuta <namimanokobuta831@gmail.com>
2019-10-30 05:19:50 +0000

    Add a program

---
+++ kadai-3/index1.js
@@ -0,0 +1,14 @@
+const day=parseInt(process.argv[2])
+
+ if (day % 3 ===0) {
+     console.log('あなたは甘えん坊な性格です')
+ } else if(day % 3 === 1) {
+     console.log('あなたは温厚な性格です')
+ } else if(day % 3 ===2){
+     console.log('あなたは生意気な性格です')
+ }
+    
+
+
+
+    
