7bb8f02119ba219285ff9799bb27ce0c217c95c0
namimanokoobuta <namimanokobuta831@gmail.com>
2019-11-20 04:16:01 +0000

    add html

---
+++ kadai-5/index.js
@@ -0,0 +1,11 @@
+const profile = {
+    firstName:'tomoya',
+    familyName:'kita',
+    age:26,
+    major:'computer science',
+    hobby:'soccer',
+    job:'student',
+    families:'2cats',
+}
+
+console.log(profile.firstName) // 'tomoya'
